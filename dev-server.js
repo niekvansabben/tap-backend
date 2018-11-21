ebfeb3467b22db0299f7fc1295c6c25bf2f610f9
Niek van Sabben <niekvansabben@gmail.com>
2018-11-21 14:16:45 +0200

    updated full route info return

--- dev-server.js
+++ dev-server.js
@@ -4,7 +4,7 @@ var url = require('url');
 var fs = require('fs');
 
 
-var debug = true;
+var debug = false;
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -22,11 +22,6 @@ con.connect(function(err) {
 
   console.log('connected as id ' + con.threadId);
 });
-
-con.query("SELECT * FROM routes", function (err, result) {
-    if (err) throw err;
-    console.log(result);
-  });
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
@@ -64,12 +59,41 @@ http.createServer(function (req, res) {
         
         if (q.query.id != null && Number.isInteger(Number(q.query.id))) {
             
-            con.query("SELECT * FROM routes WHERE id = ?;", [q.query.id], function (err, result) {
+            var sql = "SELECT * FROM routes WHERE id = ?;";
+            con.query(sql, [q.query.id], function (err, route_result) {
 
-                if (err) throw err;
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.end(JSON.stringify(result));    
-                console.log(result);
+                          
+                var sql = "SELECT point_id, description, hint, latitude, longitude FROM route_points JOIN points ON route_points.point_id = points.id WHERE route_id = ? ORDER BY order_number ASC;";
+                con.query(sql, [q.query.id], function (err, points_result) {
+                    
+                    var sql = "SELECT avg(`rating`) AS rating FROM ratings WHERE route_id = ? GROUP BY route_id;";
+                    con.query(sql, [q.query.id], function (err, rating_result) {
+
+                        if(debug){
+                            console.log(route_result);
+                            console.log(points_result);
+                            console.log(rating_result);
+                        }
+
+                        if (err) throw err;
+                        res.writeHead(200, {'Content-Type': 'application/json'});
+                        var final_result = {};
+                        final_result["id"] = route_result[0].id;
+                        final_result["name"] = route_result[0].name;
+                        final_result["description"] = route_result[0].description;
+                        final_result["duration_time"] = route_result[0].duration_time;
+                        final_result["duration_distance"] = route_result[0].duration_distance;
+                        final_result["rating"] = rating_result[0].rating;
+                        final_result["points"] = points_result;
+                        if(debug){
+                            console.log(final_result);
+                        }
+                        res.end(JSON.stringify(final_result));
+
+
+                    });
+
+                });
 
             });
             
@@ -94,4 +118,4 @@ http.createServer(function (req, res) {
     res.write("404 Not Found");
     res.end();
   }
-}).listen(2121);
+}).listen(2122);
